9ef78628ed5f2c266babe660d3f6332c93e6b557
Roman <65417719+RomanBaukin@users.noreply.github.com>
2020-09-21 15:48:22 +0400

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button_next",
+    prevEl: ".slider-button_prev",
+  },
+});

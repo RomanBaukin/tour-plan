bec9c7e7a098e80787f59d5ee489585e47e83833
Roman <65417719+RomanBaukin@users.noreply.github.com>
2020-10-02 15:59:21 +0400

    create: sending requests from a modal window

--- send.php
+++ send.php
@@ -11,13 +11,7 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // Формирование самого письма
-if ((isset($_POST['email']))) {
-  $title = "Новая заявка на подписку";
-  $body = "
-<h2>Новая заявка</h2>
-<b>E-mail:</b> $email<br>
-";
-} else {
+if ((!isset($_POST['email']))) {
   $title = "Новое обращение Best Tour Plan";
   $body = "
 <h2>Новое обращение</h2>
@@ -25,7 +19,23 @@ if ((isset($_POST['email']))) {
 <b>Телефон:</b> $phone<br><br>
 <b>Сообщение:</b><br>$message
 ";
+} elseif ((isset($_POST['name'])) && (isset($_POST['phone'])) && (isset($_POST['email'])) && (isset($_POST['message']))) {
+  $title = "Новая заяка из модального окна";
+  $body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br>
+<b>E-mail:</b> $email<br><br>
+<b>Сообщение:</b><br>$message
+";
+} else {
+  $title = "Новая заявка на подписку";
+  $body = "
+<h2>Новая заявка</h2>
+<b>E-mail:</b> $email<br>
+";
 }
+
 
 // Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
